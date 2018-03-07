039759bfefed34d5025677e1c86bd7448ee6fe5d
Allen Mathew <amathew0236@jh-mba158-15.d214.org>
2018-03-07 11:22:31 -0600

    Added a switch statement to question data swift file.

--- PersonalityQuiz/QuestionData.swift
+++ PersonalityQuiz/QuestionData.swift
@@ -21,3 +21,19 @@ struct Answer {
     var text: String
     var type: AnimalType
 }
+
+enum AnimalType: Character {
+    case dog = "🐶", rabbit = "🐰", turtle = "🐢", cat = "🐱"
+    var definition: String {
+        switch self {
+        case .dog:
+            return "You are incredibly outgoing. You surrounfd yourself with the people you love, and enjoy activites wiht your friends."
+        case .cat:
+            return "Mischievous, yet mild-tempered, you enjoy doing things on your own terms."
+        case .rabbit:
+            return "You love everything that's soft. Yoi are healthy and full of energy."
+        case.turtle:
+            return "You are wise beyond your years, and you focus on the details. Slow and steady wins the race."
+        }
+    }
+}
